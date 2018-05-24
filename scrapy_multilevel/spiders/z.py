d8dc9739654826f482509cb86ccdcf780f8004fa
lsl <songlin.lin@yundata.com>
2018-05-24 16:49:06 +0800

    增加爬取记录

--- scrapy_multilevel/spiders/z.py
+++ scrapy_multilevel/spiders/z.py
@@ -3,12 +3,14 @@ import time
 import scrapy
 
 
-def walk(lst):
+def walk(lst, levels):
     if lst:
         i = lst.pop(0)
         yield i['url']
-        yield from walk(i.get('extra'))
-        yield from walk(lst)
+        levels[0] += 1
+        yield from walk(i.get('extra'), levels)
+        levels[0] -= 1
+        yield from walk(lst, levels)
 
 
 class ZSpider(scrapy.Spider):
@@ -25,19 +27,30 @@ class ZSpider(scrapy.Spider):
                     {'url': 'http://127.0.0.1:5000/get?num=4'},
                     ]}
         req = scrapy.Request(url=item['url'], dont_filter=True)
-        req.meta['w'] = walk(item['extra'])
+        levels = [0]
+        req.meta['w'] = walk(item['extra'], levels)
+        req.meta['levels'] = levels
         yield req
    
     def parse(self, response):
         w = response.meta['w']
         bodys = response.meta.get('bodys', '')
-        #response = response.meta.get('responses', [])
+        responses = response.meta.get('responses', [])
+        print(responses)
         bodys += response.body_as_unicode()
         try:
+            levels = response.meta['levels']
+            level = levels[0]
             url = next(w)
+            if level < levels[0]:
+                responses.append(response)
+            elif level > levels[0]:
+                responses.pop()
             req = scrapy.Request(url=url, dont_filter=True)
+            req.meta['responses'] = responses
             req.meta['bodys'] = bodys
             req.meta['w'] = w
+            req.meta['levels'] = levels
             yield req
         except StopIteration as e:
             yield {'body': bodys}
